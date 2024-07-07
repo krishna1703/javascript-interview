7a57cf23978663ad44e1281310b9fc2f8d76f6d5
krishna kumar <118750903+krishna1703@users.noreply.github.com>
2024-07-07 09:22:15 +0000

    type convertion done

---
+++ js-basic/convertion.js
@@ -0,0 +1,30 @@
+let score = 33 ;
+// console.log(typeof score)
+let score1 = undefined ;
+// console.log(typeof score1)
+
+ let value = Number(score1);
+  let value2 = String(score);
+//  console.log(typeof value2)
+//  console.log(value2)
+// "33" convert number 33 type of number 
+// "333gvgh" convert number NAN type of number 
+ let krishna = "kjhluj" ;
+ let krishna2= Boolean(krishna);
+
+//  1 convert to Boolean true 
+//  0 convert to Boolean false 
+//  "" convert to Boolean false
+//  "jgvg"  convert to Boolean true
+
+//  console.log(krishna2)
+//  console.log(typeof krishna2)
+
+ let bill =  true;
+ let bills = String(bill)
+
+//  33 to convert in to string 33 but typeof  string 
+
+
+ console.log(bills)
+ console.log(typeof bills)
