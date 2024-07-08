6396555c454b4f9df1c0e779b972d6a8602c0466
krishna kumar <118750903+krishna1703@users.noreply.github.com>
2024-07-08 10:04:15 +0000

    non primitive and primitive data type

--- js-basic/dataTypes.js
+++ js-basic/dataTypes.js
@@ -9,6 +9,11 @@
 
 console.log("kknjnj")
 //  datatype
+// primitive and non primitive data type ok
+
+
+// primitive datatype
+
 
 // number = 2 to power 53;
 // bigint => more then 2 to power 53;
@@ -18,8 +23,23 @@ console.log("kknjnj")
 // undefined => undefined 
 // symbol => unique 
 
+//  non primitive datatype
 
-//  object 
+//  object  , array  , funtion
+
+let arr = ["54" , "46" , "784"]
+const fun = function(){
+   console.log("hiii")
+}
+
+ let object = {
+ name :"krishna",
+ class :"btech"
+ }
+
+//  console.log(typeof arr )  datatypes object 
+//  console.log(typeof fun )  datatypes  funtion (funtion object )
+//  console.log(typeof object) datatypes object 
 
 //  console.log(typeof "krishna");
  
@@ -34,3 +54,12 @@ console.log("kknjnj")
 //  console.log(typeof undefined);
  
 //  console.log(typeof Symbol);
+
+//  let Id = Symbol("123")
+//  let AccId = Symbol("123")
+// // console.log(Id == AccId)
+//  let bigNumber = 5156545545558465465165
+//  let BigNu = 60215665165165545;
+//  let sum = BigInt(bigNumber + BigNu) 
+//  console.log(sum)
+// //  5156605761223630258176n
