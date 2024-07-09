6939804688b001653fa1a04e2e2ca83475d05e9c
krishna kumar <118750903+krishna1703@users.noreply.github.com>
2024-07-09 06:33:11 +0000

    num and math

---
+++ js-basic/num-math.js
@@ -0,0 +1,24 @@
+  let num = 300;
+//   console.log(num)
+//    value = 300  
+  let Num  =  new Number(300)
+  console.log(Num);
+//    value =  in Number - 300
+
+// console.log( Num.toString.length);
+
+ let  sum =  654.21
+
+//  console.log(sum.toPrecision(10))
+//  The toPrecision() method in JavaScript is used to format a number to a specified length, 
+// 654.2100000
+let sum1 = 1000.0021
+// console.log(sum1.toFixed(1))
+1000.0
+// The toFixed() method in JavaScript is used to format a number using fixed-point notation
+
+let sum2 = 100000000;
+console.log(sum2.toLocaleString()) 
+// 100,000,000 for usa
+console.log(sum2.toLocaleString('en-In'))
+// 10,00,00,000 for In 
