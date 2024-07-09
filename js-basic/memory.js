eea029c77f4bdea2d0b54becc66f69f574532918
krishna kumar <118750903+krishna1703@users.noreply.github.com>
2024-07-09 05:50:43 +0000

    Heap / stack

--- js-basic/memory.js
+++ js-basic/memory.js
@@ -2,14 +2,18 @@
 
 // ### Primitive Types
 // - **Examples**: `String`, `Number`, `Boolean`, `Null`, `Undefined`, `Symbol`, `BigInt`
+
 // - **Characteristics**:
+
 //   - **Immutable**: Cannot be changed.
 //   - **Stored in Stack**.
 //   - **Copied by Value**.
 
 // ### Non-Primitive Types
 // - **Examples**: `Object`, `Array`, `Function`
+
 // - **Characteristics**:
+
 //   - **Mutable**: Can be changed.
 //   - **Stored in Heap**.
 //   - **Copied by Reference**.
