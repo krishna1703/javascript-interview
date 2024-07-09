dd3b5a4d503a9044e7de6de00361a2929fb87fc5
krishna kumar <118750903+krishna1703@users.noreply.github.com>
2024-07-09 09:30:31 +0000

    TIME AND DATE

---
+++ js-basic/time-and-date.js
@@ -0,0 +1,38 @@
+// //  *********************** date   *********************
+
+//  let num  =  new Date()
+
+// //  console.log(num)
+// //  2024-07-09T09:18:13.042Z
+// //   data type object 
+
+// // console.log(num.toString())
+// //  Tue Jul 09 2024 09:17:52 GMT+0000 (Coordinated Universal Time)
+ 
+// //  console.log(num.toDateString())
+// //  Tue Jul 09 2024   date only 
+
+// //  console.log(num.toLocaleString())
+// //  7/9/2024, 9:19:44 AM   date + time 
+
+//  let x = new Date(2000,1,25)
+// //  console.log(x)
+// //  2000-02-25T00:00:00.000Z
+// let X = new Date("02-04-2024")
+// // console.log(X)
+// // 2024-02-04T00:00:00.000Z
+
+// //   ***********************time***********************  
+
+//  let B = Date.now()
+// //  console.log(B)
+// //  1720517081718 ms from 1 jan 1970 
+
+//  let C1 =  new Date().getDate()
+
+
+//  let C2 =  new Date().getDay()
+//  let C3 =  new Date().getTime()
+// console.log(C1)
+//   console.log(C2) 
+//   console.log(C3)
