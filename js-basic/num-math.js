e5a28f40f8ee34db257051c382cdaf38861e7cd8
krishna kumar <118750903+krishna1703@users.noreply.github.com>
2024-07-09 07:02:40 +0000

    num-math

--- js-basic/num-math.js
+++ js-basic/num-math.js
@@ -1,24 +1,48 @@
-  let num = 300;
+//   let num = 300;
 //   console.log(num)
 //    value = 300  
-  let Num  =  new Number(300)
-  console.log(Num);
+//   let Num  =  new Number(300)
+//   console.log(Num);
 //    value =  in Number - 300
 
-// console.log( Num.toString.length);
+//  console.log( Num.toString.length);
 
- let  sum =  654.21
+//  let  sum =  654.21
 
-//  console.log(sum.toPrecision(10))
-//  The toPrecision() method in JavaScript is used to format a number to a specified length, 
-// 654.2100000
-let sum1 = 1000.0021
-// console.log(sum1.toFixed(1))
-1000.0
-// The toFixed() method in JavaScript is used to format a number using fixed-point notation
+//   console.log(sum.toPrecision(10))
 
-let sum2 = 100000000;
-console.log(sum2.toLocaleString()) 
-// 100,000,000 for usa
-console.log(sum2.toLocaleString('en-In'))
-// 10,00,00,000 for In 
+//   The toPrecision() method in JavaScript is used to format a number to a specified length, 
+
+//  654.2100000
+// let sum1 = 1000.0021
+
+//  console.log(sum1.toFixed(1))
+// 1000.0
+
+//  The toFixed() method in JavaScript is used to format a number using fixed-point notation
+
+// let sum2 = 100000000;
+
+// console.log(sum2.toLocaleString()) 
+
+//  100,000,000 for usa
+// console.log(sum2.toLocaleString('en-In'))
+//  10,00,00,000 for In 
+
+//   ****************** Maths *********************
+ console.log(Math.abs(-5))
+//  ignore the (-)
+//  console.log(Math.round(4.66))
+//  console.log(Math.ceil(4.2))
+//  console.log(Math.floor(4.9))
+//  console.log(Math.min(1,0,2,3,4,5,6))
+//  console.log(Math.max(9,8,7,6,5,43,2,1))
+
+//   console.log(Math.random())
+//     value  0 to 1 deta hai  = 0.1003466670432076
+
+//  console.log(Math.floor(Math.random()*100))
+
+//   let min = 10 ;
+//   let max = 20;
+//   console.log(Math.floor(Math.random() * (max-min +1 ) ) + min )
