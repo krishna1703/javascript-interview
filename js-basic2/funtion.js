f0d38ccffe64e2ae07d1bee73f0d99fe82604a21
krishna kumar <118750903+krishna1703@users.noreply.github.com>
2024-07-11 09:55:03 +0000

    funtion

---
+++ js-basic2/funtion.js
@@ -0,0 +1,71 @@
+
+function Add(parameters){
+//  *********** some line of code /********** */
+}
+
+//  Add("arguments")
+//   function call
+//   add tow number 
+
+// function AddTwoNUM(number1,number2){
+//  const sum  = number1 + number2
+//   console.log(sum)
+// }
+// AddTwoNUM(5,10)
+// // 15
+ 
+
+// function AddTwoNUM(number1,number2){
+//   const sum  = number1 + number2
+//    console.log(sum)
+//  }
+//   const sum  =  AddTwoNUM(5,10)
+//   console.log("sum of two number :" ,sum );
+
+  // result
+  // sum of two number : undefined
+
+  // function AddTwoNUM(number1,number2){
+  //   // const sum  = number1 + number2
+  //   //  console.log(sum)
+  //   //  return sum
+  //  return number1 + number2
+   
+  //  }
+  //   const sum1  =  AddTwoNUM(5,10)
+  //   console.log("sum of two number :" ,sum1 );
+
+    // result
+    // sum of two number : 15
+
+//      function login(user){
+// return `${user} just login hua hai `
+//      }
+
+//     console.log(login())
+    // result
+    // undefined just login hua hai 
+
+    // function login1(user){
+    //   if(!user){
+    //     console.log("enter your name")
+    //     return;
+    //   }
+    //   return `${user} just login hua hai `
+    //        }
+      
+    //       // console.log(login1())
+    //        console.log(login1("kkkk"))
+
+           function login1(user = "krishna"){
+            if(!user){
+              console.log("enter your name")
+              return;
+            }
+            return `${user} just login hua hai `
+                 }
+            
+                // console.log(login1())
+                 console.log(login1("jknjjnj"))
+
+                //  user = "krishna" overwrite the user value to  this "jknjjnj"
