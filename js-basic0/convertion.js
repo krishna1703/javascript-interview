e3656a3422cdc47ce8b4334cf838ae1257cd60f7
krishna kumar <118750903+krishna1703@users.noreply.github.com>
2024-07-11 08:51:19 +0000

    js-basic

---
+++ js-basic0/convertion.js
@@ -0,0 +1,73 @@
+let score = 33 ;
+// console.log(typeof score)
+let score1 = undefined ;
+// console.log(typeof score1)
+
+ let value = Number(score1);
+  let value2 = String(score);
+//  console.log(typeof value2)
+//  console.log(value2)
+// "33" convert number 33 type of number 
+// "333gvgh" convert number NAN type of number 
+ let krishna = "kjhluj" ;
+ let krishna2= Boolean(krishna);
+
+//  1 convert to Boolean true 
+//  0 convert to Boolean false 
+//  "" convert to Boolean false
+//  "jgvg"  convert to Boolean true
+
+//  console.log(krishna2)
+//  console.log(typeof krishna2)
+
+ let bill =  true;
+ let bills = String(bill)
+
+//  33 to convert in to string 33 but typeof  string 
+
+
+//  console.log(bills)
+//  console.log(typeof bills)
+ 
+//  JavaScript Operators
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2/2);
+//  console.log(2%2); 
+//  console.log(2**2);
+
+//  let str1 = "hello"
+//  let str2 = " krishna"
+//  let str3 = str1+str2
+//  console.log(str3);
+
+//   console.log("1"+2);
+//   console.log(1+"2");
+//   console.log("1" + 2 + 2 + 5 +5+5);
+//   //  if frist string then treat as  string 
+//   console.log(1+2+"3");
+  //  if frist is number then treat as number 
+//  console.log(+true) = 1
+// console.log(+"") = 0 
+// console.log(+"hjvtyv")  = NaN
+
+  // let counter = 99;
+
+  // // ++counter prefix  increase phle hoti h
+  // //  counter++ postfix   bad  me   increase hoti hai  
+  // console.log(counter);
+
+
+let x = 3;
+const y = x++;
+
+console.log(x , y);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(a,b);
+// Expected output: "a:4, b:4"
