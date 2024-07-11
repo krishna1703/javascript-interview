5e1a3b6d652c7cc5c31e13a7cfe6f0b9c09eb60c
krishna kumar <118750903+krishna1703@users.noreply.github.com>
2024-07-11 10:20:08 +0000

    funtions

--- js-basic2/funtion.js
+++ js-basic2/funtion.js
@@ -1,7 +1,7 @@
 
-function Add(parameters){
-//  *********** some line of code /********** */
-}
+// function Add(parameters){
+// //  *********** some line of code /********** */
+// }
 
 //  Add("arguments")
 //   function call
@@ -57,15 +57,57 @@ function Add(parameters){
     //       // console.log(login1())
     //        console.log(login1("kkkk"))
 
-           function login1(user = "krishna"){
-            if(!user){
-              console.log("enter your name")
-              return;
-            }
-            return `${user} just login hua hai `
-                 }
+          //  function login1(user = "krishna"){
+          //   if(!user){
+            //   console.log("enter your name")
+            //   return;
+            // }
+            // return `${user} just login hua hai `
+            //      }
             
-                // console.log(login1())
-                 console.log(login1("jknjjnj"))
+            //     // console.log(login1())
+            //      console.log(login1("jknjjnj"))
 
                 //  user = "krishna" overwrite the user value to  this "jknjjnj"
+
+           
+
+//                 function calculate(money){
+// return money;
+
+//                 }
+//        console.log(calculate(100)) 
+      //   result 100 
+
+           // ...rest opr. after using rest opt.
+
+          //  function calculate(...money){
+          //   return money;
+            
+          //                   }
+          //          console.log(calculate(100,200,300,400,500))
+                  //  [ 100, 200, 300, 400, 500 ] result 
+
+                  // /object in function
+
+                  //  const S ={
+                  //   name:"kkkk",
+                  //   age:555,
+                  //   wife:"no"
+                
+                  //  }
+                   
+
+//                    function  details(anytypesobject){
+// return `nameis:${anytypesobject.name} ,age is : ${anytypesobject.age}`
+//                    }
+                   
+//                    console.log(details(S))
+
+// array in funtion
+
+// const num = [1,2,3,4,5,6,7];
+// function  Array(getarray){
+// return getarray[5]
+// }
+//     console.log(Array(num))
